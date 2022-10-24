ea072e0455e17577a3759009705961acefb2b8e3
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-10-24 14:45:23 +0300

    Added main method to process the insert
    Added main method to read username,password,email from console and insert them if they are valid!

--- src/InsertUser.java
+++ src/InsertUser.java
@@ -10,8 +10,56 @@ public class InsertUser {
 
     private static final String INSERT_USER = "CALL insertUser(?,?,?);";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = Utils.getSQLConnection();
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine().trim();
+
+        if (!isUsernameValid(username)) {
+            System.out.println(Constants.INVALID_USERNAME);
+            return;
+        }
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine().trim();
+
+
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine().trim();
+
+        if (!isEmailValid(email)) {
+            System.out.println(Constants.INVALID_EMAIL);
+            return;
+        }
+
+        PreparedStatement insertStatement = connection.prepareStatement(INSERT_USER);
+
+        insertStatement.setString(1,username);
+        insertStatement.setString(2,password);
+        insertStatement.setString(3,email);
+
+        try {
+            ResultSet resultSet = insertStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                System.out.println("Error! Please, try again!");
+                connection.close();
+                return;
+            }
+
+            String registeredUsername = resultSet.getString(Constants.COLUMN_LABEL_USERNAME);
+            String registeredEmail = resultSet.getString(Constants.COLUMN_LABEL_EMAIL);
+            System.out.printf("Successfully Registered User: %s with Email: %s",
+                    registeredUsername, registeredEmail);
+
+        }catch (SQLException sqlException){
+            if (sqlException.getErrorCode() == 1062){
+                System.out.println("Such user already exist!");
+            }
+        }
     }
 
     private static boolean isUsernameValid(String username){
