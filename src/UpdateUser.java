7c16d2a8be62c58fc5b87a5b61175242bfdd9b06
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-10-24 16:28:06 +0300

    Update user Email implemented

--- src/UpdateUser.java
+++ src/UpdateUser.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class UpdateUser {
 
     private static final String UPDATE_EMAIL =
@@ -10,6 +15,44 @@ public class UpdateUser {
                     "WHERE id = ?";
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("User ID: ");
+        String userIdStr = scanner.nextLine();
+
+        if (!Validator.isInteger(userIdStr)) {
+            System.out.println(Constants.INVALID_USER_ID);
+            return;
+        }
+        int userId = Integer.parseInt(userIdStr);
+
+        System.out.print("New Email: ");
+        String email = scanner.nextLine();
+
+        if (!Validator.isValidEmail(email)){
+            System.out.println(Constants.INVALID_EMAIL);
+            return;
+        }
+
+        try {
+            Connection connection = Utils.getSQLConnection();
+            PreparedStatement statement = connection.prepareStatement(UPDATE_EMAIL);
+            statement.setString(1,email);
+            statement.setInt(2,userId);
+
+            final int updated = statement.executeUpdate();
+
+            if (updated == 0){
+                System.out.println(Constants.EMAIL_NOT_UPDATED);
+                connection.close();
+                return;
+            }
+            System.out.println(Constants.EMAIL_SUCCESSFULLY_UPDATED);
+            connection.close();
+
+        }catch (SQLException sqlException){
+            System.out.println("Error! " + sqlException.getMessage());
+        }
+
     }
 }
