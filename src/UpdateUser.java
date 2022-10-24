25c626e80fbc5b4975d1a97feae40637bd412124
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-10-24 15:16:41 +0300

    Added update email SQL query

--- src/UpdateUser.java
+++ src/UpdateUser.java
@@ -1,2 +1,11 @@
 public class UpdateUser {
+
+    private static final String UPDATE_EMAIL =
+            "UPDATE users " +
+                    "SET email = ? " +
+                    "WHERE id = ?";
+
+    public static void main(String[] args) {
+        
+    }
 }
